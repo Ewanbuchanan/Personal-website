519cde9cb354bd8200e2110ce77f2f8b120fd9de
Ewanbuchanan <ewanbuchanan8@gmail.com>
2024-02-19 15:18:26 -0800

    g

---
+++ Index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export function index() {
+return <div>
+    <React.Fragment>
+        <meta charSet= "UTF-8"/>
+        <title>Ewan Buchanan</title>
+    </React.Fragment>
+    <body>
+
+    </body>
+</div>
+}
+index()
